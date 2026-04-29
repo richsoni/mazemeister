e2580942fae7382314b09ae516fa5f244989bfd7
Rich Soni <rich@richsoni.com>
2026-04-28 21:27:32 -0400

    fix height of static enemies

--- enemies/static.js
+++ enemies/static.js
@@ -2,7 +2,7 @@ import { Enemy } from './Enemy.js';
 
 export class Hole extends Enemy {
   static templateSymbol = 'H';
-  static renderSymbol   = '■';
+  static renderSymbol   = '█';
   static color          = 'red';
   static movement       = 'hole';
   // move and getSymbol inherited from Enemy (no-op move, renderSymbol)
